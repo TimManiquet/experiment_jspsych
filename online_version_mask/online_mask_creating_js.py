b8af1fe137938344b1dd2820a73d15d82e512016
TimManiquet <tim.maniquet@kuleuven.be>
2022-10-21 15:55:06 +0200

    mask version online

---
+++ online_version_mask/online_mask_creating_js.py
@@ -0,0 +1,180 @@
+####################
+# This script takes in all the images in 'task_img' and outputs a .json file.
+# The output will contain (1) the filename (./task_img/filename.png), (2) the category
+# that the image belongs to, and (3) the manipulation applied.
+#
+# With masking: the script will also add the name of the mask
+# to be applied for each image.
+#
+# This script does the same with images in 'train_img'.
+####################
+import pathlib
+import glob
+import pandas as pd
+import csv
+import json 
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+
+files = glob.glob('./online_version_mask/task_img/*.png')
+mask_files = glob.glob('./online_version_mask/mask_img/*.png')
+
+filenames = []
+mask_filenames = []
+category = []
+manipulation = []
+
+
+# train_img/lightcluttermatched_person1905.png
+# https://www.psytests.be/timages/instructions_img/control_person1.png
+
+for i in range(len(files)):
+    # adding the name of the file to the list
+    file = pathlib.Path(files[i])
+    filenames.append("https://www.psytests.be/timages/" + str(file.parts[-2]) + '/' + file.name)
+    mask_filename = pathlib.Path(mask_files[i])
+    # filename.append("https://www.psytests.be/timages/" + str(file.parents[0]) + '/' + file.name)
+    mask_filenames.append(str(mask_filename.parts[-2]) + '/' + mask_filename.name)
+
+    # adding the category and correct response
+    if 'person' in str(file.name):
+        category.append('person')
+        # correct_response.append('l')
+    elif 'cat' in str(file.name):
+        category.append('cat')
+        # correct_response.append('j')
+    elif 'bird' in str(file.name):
+        category.append('bird')
+        # correct_response.append('d')
+    elif 'banana' in str(file.name):
+        category.append('banana')
+        # correct_response.append('q')
+    elif 'firehydrant' in str(file.name):
+        category.append('firehydrant')
+        # correct_response.append('s')
+    elif 'bus' in str(file.name):
+        category.append('bus')
+        # correct_response.append('k')
+    elif 'building' in str(file.name):
+        category.append('building')
+        # correct_response.append('f')
+    elif 'tree' in str(file.name):
+        category.append('tree')
+        # correct_response.append('m')
+    
+    # adding the manipulation
+    if 'blob' in str(file.name):
+        manipulation.append('occlusion')
+    elif 'control' in str(file.name):
+        manipulation.append('control')
+    elif 'clutter' in str(file.name):
+        manipulation.append('clutter')
+    elif 'lp' in str(file.name):
+        manipulation.append('low-pass')
+    elif 'hp' in str(file.name):
+        manipulation.append('high-pass')
+
+# csvFilePath = r'data.csv'
+# jsonFilePath = r'data.json'
+# csv_to_json(csvFilePath, jsonFilePath)
+
+# creating a dictionary that includes all the lists
+# dict = {"filename": filename,"category": category, "manipulation": manipulation, "correct_response": correct_response}
+dict = {"filename": filenames, "mask_filename": mask_filenames, "category": category, "manipulation": manipulation}
+# transforming it to a dataframe
+df = pd.DataFrame(dict)
+df.to_csv('./online_version_mask/img_stimuli.csv')
+csv_to_json('./online_version_mask/img_stimuli.csv', './online_version_mask/img_stimuli.js')
+
+####################################
+# Once the img_stimuli.js file is created, remember to add 'var img_stimuli = ' on the first line, so JsPsych can read it.
+####################################
+
+
+train_files = glob.glob('./online_version_mask/train_img/*.png')
+train_mask_files = glob.glob('./online_version_mask/train_mask_img/*.png')
+
+train_filenames = []
+train_mask_filenames = []
+train_category = []
+train_manipulation = []
+# correct_response = []
+
+for i in range(len(train_files)):
+    # adding the name of the file to the list
+    train_file = pathlib.Path(train_files[i])
+    train_filenames.append("https://www.psytests.be/timages/" + str(train_file.parts[-2]) + '/' + train_file.name)
+    train_mask_filename = pathlib.Path(mask_files[i])
+    # filename.append("https://www.psytests.be/timages/" + str(file.parents[0]) + '/' + file.name)
+    train_mask_filenames.append(str(train_mask_filename.parts[-2]) + '/' + train_mask_filename.name)
+
+    # adding the category and correct response
+    if 'person' in str(train_file.name):
+        train_category.append('person')
+        # correct_response.append('l')
+    elif 'cat' in str(train_file.name):
+        train_category.append('cat')
+        # correct_response.append('j')
+    elif 'bird' in str(train_file.name):
+        train_category.append('bird')
+        # correct_response.append('d')
+    elif 'banana' in str(train_file.name):
+        train_category.append('banana')
+        # correct_response.append('q')
+    elif 'firehydrant' in str(train_file.name):
+        train_category.append('firehydrant')
+        # correct_response.append('s')
+    elif 'bus' in str(train_file.name):
+        train_category.append('bus')
+        # correct_response.append('k')
+    elif 'building' in str(train_file.name):
+        train_category.append('building')
+        # correct_response.append('f')
+    elif 'tree' in str(train_file.name):
+        train_category.append('tree')
+        # correct_response.append('m')
+    
+    # adding the manipulation
+    if 'blob' in str(train_file.name):
+        train_manipulation.append('occlusion')
+    elif 'control' in str(train_file.name):
+        train_manipulation.append('control')
+    elif 'clutter' in str(train_file.name):
+        train_manipulation.append('clutter')
+    elif 'lp' in str(train_file.name):
+        train_manipulation.append('low-pass')
+    elif 'hp' in str(train_file.name):
+        train_manipulation.append('high-pass')
+
+# csvFilePath = r'data.csv'
+# jsonFilePath = r'data.json'
+# csv_to_json(csvFilePath, jsonFilePath)
+
+# creating a dictionary that includes all the lists
+# dict = {"filename": filename,"category": category, "manipulation": manipulation, "correct_response": correct_response}
+train_dict = {"train_filename": train_filenames,"train_mask_filename": train_mask_filenames, "train_category": train_category, "train_manipulation": train_manipulation}
+# transforming it to a dataframe
+train_df = pd.DataFrame(train_dict)
+train_df.to_csv('./online_version_mask/train_img_stimuli.csv')
+csv_to_json('./online_version_mask/train_img_stimuli.csv', './online_version_mask/train_img_stimuli.js')
+
+
+####################################
+# Once the train_img_stimuli.js file is created, remember to add 'var train_img_stimuli = ' on the first line, so JsPsych can read it.
+####################################
---
+++ online_version_mask/train_img_stimuli.js
@@ -0,0 +1,394 @@
+var train_img_stimuli = [
+    {
+        "": "0",
+        "train_filename": "https://www.psytests.be/timages/train_img/lp_matched_person1905.png",
+        "train_mask_filename": "mask_img/000000353999.png",
+        "train_category": "person",
+        "train_manipulation": "low-pass"
+    },
+    {
+        "": "1",
+        "train_filename": "https://www.psytests.be/timages/train_img/blobhighmatched_firehydrant46.png",
+        "train_mask_filename": "mask_img/000000336668.png",
+        "train_category": "firehydrant",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "2",
+        "train_filename": "https://www.psytests.be/timages/train_img/lightcluttermatched_person1905.png",
+        "train_mask_filename": "mask_img/000000365556.png",
+        "train_category": "person",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "3",
+        "train_filename": "https://www.psytests.be/timages/train_img/blobhighmatched_person1905.png",
+        "train_mask_filename": "mask_img/000000358833.png",
+        "train_category": "person",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "4",
+        "train_filename": "https://www.psytests.be/timages/train_img/lp_matched_tree2.png",
+        "train_mask_filename": "mask_img/000000363325.png",
+        "train_category": "tree",
+        "train_manipulation": "low-pass"
+    },
+    {
+        "": "5",
+        "train_filename": "https://www.psytests.be/timages/train_img/lp_matched_firehydrant46.png",
+        "train_mask_filename": "mask_img/000000329576.png",
+        "train_category": "firehydrant",
+        "train_manipulation": "low-pass"
+    },
+    {
+        "": "6",
+        "train_filename": "https://www.psytests.be/timages/train_img/hp_matched_person1905.png",
+        "train_mask_filename": "mask_img/000000365768.png",
+        "train_category": "person",
+        "train_manipulation": "high-pass"
+    },
+    {
+        "": "7",
+        "train_filename": "https://www.psytests.be/timages/train_img/heavycluttermatched_person1905.png",
+        "train_mask_filename": "mask_img/000000337827.png",
+        "train_category": "person",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "8",
+        "train_filename": "https://www.psytests.be/timages/train_img/control_banana3666.png",
+        "train_mask_filename": "mask_img/000000346576.png",
+        "train_category": "banana",
+        "train_manipulation": "control"
+    },
+    {
+        "": "9",
+        "train_filename": "https://www.psytests.be/timages/train_img/bloblowmatched_bird259.png",
+        "train_mask_filename": "mask_img/000000345719.png",
+        "train_category": "bird",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "10",
+        "train_filename": "https://www.psytests.be/timages/train_img/hp_matched_cat147.png",
+        "train_mask_filename": "mask_img/000000346589.png",
+        "train_category": "cat",
+        "train_manipulation": "high-pass"
+    },
+    {
+        "": "11",
+        "train_filename": "https://www.psytests.be/timages/train_img/control_cat147.png",
+        "train_mask_filename": "mask_img/000000349686.png",
+        "train_category": "cat",
+        "train_manipulation": "control"
+    },
+    {
+        "": "12",
+        "train_filename": "https://www.psytests.be/timages/train_img/blobhighmatched_bird259.png",
+        "train_mask_filename": "mask_img/000000368648.png",
+        "train_category": "bird",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "13",
+        "train_filename": "https://www.psytests.be/timages/train_img/bloblowmatched_building95.png",
+        "train_mask_filename": "mask_img/000000360573.png",
+        "train_category": "building",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "14",
+        "train_filename": "https://www.psytests.be/timages/train_img/bloblowmatched_tree2.png",
+        "train_mask_filename": "mask_img/000000333665.png",
+        "train_category": "tree",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "15",
+        "train_filename": "https://www.psytests.be/timages/train_img/heavycluttermatched_cat147.png",
+        "train_mask_filename": "mask_img/000000347133.png",
+        "train_category": "cat",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "16",
+        "train_filename": "https://www.psytests.be/timages/train_img/bloblowmatched_bus180.png",
+        "train_mask_filename": "mask_img/000000326903.png",
+        "train_category": "bus",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "17",
+        "train_filename": "https://www.psytests.be/timages/train_img/heavycluttermatched_firehydrant46.png",
+        "train_mask_filename": "mask_img/000000336457.png",
+        "train_category": "firehydrant",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "18",
+        "train_filename": "https://www.psytests.be/timages/train_img/lightcluttermatched_tree2.png",
+        "train_mask_filename": "mask_img/000000347292.png",
+        "train_category": "tree",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "19",
+        "train_filename": "https://www.psytests.be/timages/train_img/lightcluttermatched_cat147.png",
+        "train_mask_filename": "mask_img/000000358359.png",
+        "train_category": "cat",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "20",
+        "train_filename": "https://www.psytests.be/timages/train_img/control_building95.png",
+        "train_mask_filename": "mask_img/000000344758.png",
+        "train_category": "building",
+        "train_manipulation": "control"
+    },
+    {
+        "": "21",
+        "train_filename": "https://www.psytests.be/timages/train_img/control_firehydrant46.png",
+        "train_mask_filename": "mask_img/000000327758.png",
+        "train_category": "firehydrant",
+        "train_manipulation": "control"
+    },
+    {
+        "": "22",
+        "train_filename": "https://www.psytests.be/timages/train_img/lp_matched_cat147.png",
+        "train_mask_filename": "mask_img/000000322807.png",
+        "train_category": "cat",
+        "train_manipulation": "low-pass"
+    },
+    {
+        "": "23",
+        "train_filename": "https://www.psytests.be/timages/train_img/bloblowmatched_banana3666.png",
+        "train_mask_filename": "mask_img/000000346367.png",
+        "train_category": "banana",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "24",
+        "train_filename": "https://www.psytests.be/timages/train_img/blobhighmatched_bus180.png",
+        "train_mask_filename": "mask_img/000000330583.png",
+        "train_category": "bus",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "25",
+        "train_filename": "https://www.psytests.be/timages/train_img/blobhighmatched_building95.png",
+        "train_mask_filename": "mask_img/000000345691.png",
+        "train_category": "building",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "26",
+        "train_filename": "https://www.psytests.be/timages/train_img/heavycluttermatched_bird259.png",
+        "train_mask_filename": "mask_img/000000327000.png",
+        "train_category": "bird",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "27",
+        "train_filename": "https://www.psytests.be/timages/train_img/control_bus180.png",
+        "train_mask_filename": "mask_img/000000362986.png",
+        "train_category": "bus",
+        "train_manipulation": "control"
+    },
+    {
+        "": "28",
+        "train_filename": "https://www.psytests.be/timages/train_img/hp_matched_bird259.png",
+        "train_mask_filename": "mask_img/000000339766.png",
+        "train_category": "bird",
+        "train_manipulation": "high-pass"
+    },
+    {
+        "": "29",
+        "train_filename": "https://www.psytests.be/timages/train_img/hp_matched_tree2.png",
+        "train_mask_filename": "mask_img/000000367785.png",
+        "train_category": "tree",
+        "train_manipulation": "high-pass"
+    },
+    {
+        "": "30",
+        "train_filename": "https://www.psytests.be/timages/train_img/lightcluttermatched_building95.png",
+        "train_mask_filename": "mask_img/000000350897.png",
+        "train_category": "building",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "31",
+        "train_filename": "https://www.psytests.be/timages/train_img/hp_matched_bus180.png",
+        "train_mask_filename": "mask_img/000000326083.png",
+        "train_category": "bus",
+        "train_manipulation": "high-pass"
+    },
+    {
+        "": "32",
+        "train_filename": "https://www.psytests.be/timages/train_img/hp_matched_banana3666.png",
+        "train_mask_filename": "mask_img/000000368307.png",
+        "train_category": "banana",
+        "train_manipulation": "high-pass"
+    },
+    {
+        "": "33",
+        "train_filename": "https://www.psytests.be/timages/train_img/bloblowmatched_cat147.png",
+        "train_mask_filename": "mask_img/000000353622.png",
+        "train_category": "cat",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "34",
+        "train_filename": "https://www.psytests.be/timages/train_img/heavycluttermatched_bus180.png",
+        "train_mask_filename": "mask_img/000000354603.png",
+        "train_category": "bus",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "35",
+        "train_filename": "https://www.psytests.be/timages/train_img/lp_matched_building95.png",
+        "train_mask_filename": "mask_img/000000342060.png",
+        "train_category": "building",
+        "train_manipulation": "low-pass"
+    },
+    {
+        "": "36",
+        "train_filename": "https://www.psytests.be/timages/train_img/lp_matched_bus180.png",
+        "train_mask_filename": "mask_img/000000341227.png",
+        "train_category": "bus",
+        "train_manipulation": "low-pass"
+    },
+    {
+        "": "37",
+        "train_filename": "https://www.psytests.be/timages/train_img/bloblowmatched_firehydrant46.png",
+        "train_mask_filename": "mask_img/000000357860.png",
+        "train_category": "firehydrant",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "38",
+        "train_filename": "https://www.psytests.be/timages/train_img/blobhighmatched_banana3666.png",
+        "train_mask_filename": "mask_img/000000353384.png",
+        "train_category": "banana",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "39",
+        "train_filename": "https://www.psytests.be/timages/train_img/blobhighmatched_tree2.png",
+        "train_mask_filename": "mask_img/000000365187.png",
+        "train_category": "tree",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "40",
+        "train_filename": "https://www.psytests.be/timages/train_img/lightcluttermatched_bus180.png",
+        "train_mask_filename": "mask_img/000000355871.png",
+        "train_category": "bus",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "41",
+        "train_filename": "https://www.psytests.be/timages/train_img/lightcluttermatched_banana3666.png",
+        "train_mask_filename": "mask_img/000000346158.png",
+        "train_category": "banana",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "42",
+        "train_filename": "https://www.psytests.be/timages/train_img/blobhighmatched_cat147.png",
+        "train_mask_filename": "mask_img/000000367425.png",
+        "train_category": "cat",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "43",
+        "train_filename": "https://www.psytests.be/timages/train_img/lightcluttermatched_firehydrant46.png",
+        "train_mask_filename": "mask_img/000000340703.png",
+        "train_category": "firehydrant",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "44",
+        "train_filename": "https://www.psytests.be/timages/train_img/hp_matched_building95.png",
+        "train_mask_filename": "mask_img/000000334938.png",
+        "train_category": "building",
+        "train_manipulation": "high-pass"
+    },
+    {
+        "": "45",
+        "train_filename": "https://www.psytests.be/timages/train_img/hp_matched_firehydrant46.png",
+        "train_mask_filename": "mask_img/000000358174.png",
+        "train_category": "firehydrant",
+        "train_manipulation": "high-pass"
+    },
+    {
+        "": "46",
+        "train_filename": "https://www.psytests.be/timages/train_img/lp_matched_banana3666.png",
+        "train_mask_filename": "mask_img/000000353948.png",
+        "train_category": "banana",
+        "train_manipulation": "low-pass"
+    },
+    {
+        "": "47",
+        "train_filename": "https://www.psytests.be/timages/train_img/heavycluttermatched_tree2.png",
+        "train_mask_filename": "mask_img/000000354570.png",
+        "train_category": "tree",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "48",
+        "train_filename": "https://www.psytests.be/timages/train_img/bloblowmatched_person1905.png",
+        "train_mask_filename": "mask_img/000000359097.png",
+        "train_category": "person",
+        "train_manipulation": "occlusion"
+    },
+    {
+        "": "49",
+        "train_filename": "https://www.psytests.be/timages/train_img/control_bird259.png",
+        "train_mask_filename": "mask_img/000000322586.png",
+        "train_category": "bird",
+        "train_manipulation": "control"
+    },
+    {
+        "": "50",
+        "train_filename": "https://www.psytests.be/timages/train_img/lp_matched_bird259.png",
+        "train_mask_filename": "mask_img/000000326092.png",
+        "train_category": "bird",
+        "train_manipulation": "low-pass"
+    },
+    {
+        "": "51",
+        "train_filename": "https://www.psytests.be/timages/train_img/heavycluttermatched_building95.png",
+        "train_mask_filename": "mask_img/000000346799.png",
+        "train_category": "building",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "52",
+        "train_filename": "https://www.psytests.be/timages/train_img/lightcluttermatched_bird259.png",
+        "train_mask_filename": "mask_img/000000334858.png",
+        "train_category": "bird",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "53",
+        "train_filename": "https://www.psytests.be/timages/train_img/control_tree2.png",
+        "train_mask_filename": "mask_img/000000354174.png",
+        "train_category": "tree",
+        "train_manipulation": "control"
+    },
+    {
+        "": "54",
+        "train_filename": "https://www.psytests.be/timages/train_img/heavycluttermatched_banana3666.png",
+        "train_mask_filename": "mask_img/000000327165.png",
+        "train_category": "banana",
+        "train_manipulation": "clutter"
+    },
+    {
+        "": "55",
+        "train_filename": "https://www.psytests.be/timages/train_img/control_person1905.png",
+        "train_mask_filename": "mask_img/000000363727.png",
+        "train_category": "person",
+        "train_manipulation": "control"
+    }
+]
